d63623f568a76bfcc05d797583c3605ab4f8adc2
Stefanos Angelidis <angelids@amazon.com>
2018-11-24 20:04:46 +0100

    added multitasking mate (mate-mt.py); minor editd to mate.py

--- mate-mt.py
+++ mate-mt.py
@@ -4,10 +4,8 @@ import sys
 import h5py
 import argparse
 from time import time
-
+from pprint import pprint
 import numpy as np
-from numpy.random import permutation, seed
-from scipy.cluster.vq import kmeans
 
 import torch
 import torch.nn as nn
@@ -15,74 +13,19 @@ import torch.nn.functional as F
 from torch.autograd import Variable
 from torch.nn.init import xavier_uniform
 from torch.nn.utils import clip_grad_norm
+from numpy.random import permutation, seed
 
+from mate import AttentionEncoder
 from loss import TripletMarginCosineLoss, OrthogonalityLoss
 
-class AttentionEncoder(nn.Module):
-    """Segment encoder that produces segment vectors as the weighted
-    average of word embeddings.
-    """
-    def __init__(self, vocab_size, emb_size, bias=True, M=None, b=None):
-        """Initializes the encoder using a [vocab_size x emb_size] embedding
-        matrix. The encoder learns a matrix M, which may be initialized
-        explicitely or randomly.
-
-        Parameters:
-            vocab_size (int): the vocabulary size
-            emb_size (int): dimensionality of embeddings
-            bias (bool): whether or not to use a bias vector
-            M (matrix): the attention matrix (None for random)
-            b (vector): the attention bias vector (None for random)
-        """
-        super(AttentionEncoder, self).__init__()
-        self.lookup = nn.Embedding(vocab_size, emb_size)
-        self.M = nn.Parameter(torch.Tensor(emb_size, emb_size))
-        if M is None:
-            xavier_uniform(self.M.data)
-        else:
-            self.M.data.copy_(M)
-        if bias:
-            self.b = nn.Parameter(torch.Tensor(1))
-            if b is None:
-                self.b.data.zero_()
-            else:
-                self.b.data.copy_(b)
-        else:
-            self.b = None
-
-    def forward(self, inputs):
-        """Forwards an input batch through the encoder"""
-        x_wrd = self.lookup(inputs)
-        x_avg = x_wrd.mean(dim=1)
-
-        x = x_wrd.matmul(self.M)
-        x = x.matmul(x_avg.unsqueeze(1).transpose(1,2))
-        if self.b is not None:
-            x += self.b
-
-        x = F.tanh(x)
-        a = F.softmax(x, dim=1)
-
-        z = a.transpose(1,2).matmul(x_wrd)
-        z = z.squeeze()
-        if z.dim() == 1:
-            return z.unsqueeze(0)
-        return z
-
-    def set_word_embeddings(self, embeddings, fix_w_emb=True):
-        """Initialized word embeddings dictionary and defines if it is trainable"""
-        self.lookup.weight.data.copy_(embeddings)
-        self.lookup.weight.requires_grad = not fix_w_emb
-
-class AspectAutoencoder(nn.Module):
-    """The aspect autoencoder class that defines our Multitask Aspect Extractor,
-    but also implements the Aspect-Based Autoencoder (ABAE), if the aspect matrix
-    not initialized using seed words
+class MultitaskAutoencoder(nn.Module):
+    """The multitasking version of our Multitask Aspect Extractor.
     """
     def __init__(self, vocab_size, emb_size, num_aspects=10, neg_samples=10,
-            w_emb=None, a_emb=None, recon_method='centr', seed_w=None, num_seeds=None,
-            attention=False, bias=True, M=None, b=None, fix_w_emb=True, fix_a_emb=False):
-        """Initializes the autoencoder instance.
+            w_emb=None, a_emb=None, recon_method='start', seed_w=None, num_seeds=None,
+            attention=False, bias=True, M=None, b=None, fix_w_emb=True, fix_a_emb=False,
+            num_domains=6, midlayer=50, dropout=0.0):
+        """Initializes the multitasking autoencoder instance.
 
         Parameters:
             vocab_size (int): the vocabulary size
@@ -105,8 +48,11 @@ class AspectAutoencoder(nn.Module):
             b (vector): bias vector for attention encoder (optional)
             fix_w_emb (bool): fix word embeddings throughout trainign
             fix_a_emb (bool): fix aspect embeddings throughout trainign
+            num_domains (int): number of product domains
+            midlayer (int): size of middle layer in domain classifier
+            dropout (float): dropout probability for domain classifier
         """
-        super(AspectAutoencoder, self).__init__()
+        super(MultitaskAutoencoder, self).__init__()
         self.vocab_size = vocab_size
         self.emb_size = emb_size
         self.recon_method = recon_method
@@ -151,13 +97,26 @@ class AspectAutoencoder(nn.Module):
         else:
             self.seed_w = None
 
+        # autoencoder
         self.lin = nn.Linear(emb_size, num_aspects)
         self.softmax = nn.Softmax(dim=1)
 
-    def forward(self, inputs, batch_num=None):
+        # domain classifier
+        if dropout > 0:
+            self.drop = nn.Dropout(p=dropout)
+        else:
+            self.drop = None
+
+        self.dlin1 = nn.Linear(emb_size, midlayer)
+        self.dlin2 = nn.Linear(midlayer, num_domains)
+
+    def forward(self, inputs, domain_mask=None, batch_num=None):
         if self.training:
             # mask used for randomly selected negative examples
-            self.cur_mask = self._create_neg_mask(inputs.size(0))
+            self.cur_neg_mask = self._create_neg_mask(inputs.size(0))
+
+            assert domain_mask is not None, 'When training, you must forward a domain mask'
+            self.cur_domain_mask = domain_mask
 
         if not self.attention:
             offsets = Variable(torch.arange(0, inputs.numel(), inputs.size(1), out=inputs.data.new().long()))
@@ -165,6 +124,7 @@ class AspectAutoencoder(nn.Module):
         else:
             enc = self.seg_encoder(inputs)
 
+        # autoencoder
         x = self.lin(enc)
         a_probs = self.softmax(x)
 
@@ -173,7 +133,7 @@ class AspectAutoencoder(nn.Module):
         elif self.recon_method == 'fix':
             a_emb_w = self.a_emb.mul(self.seed_w.view(self.num_aspects, self.num_seeds, 1))
             r = a_probs.view(-1, self.num_aspects, 1, 1).mul(a_emb_w).sum(dim=2).sum(dim=1)
-        elif self.recon_method == 'init':
+        elif self.recon_method == 'init' or self.recon_method == 'rand':
             seed_w_norm = F.softmax(self.seed_w, dim=1)
             a_emb_w = self.a_emb.mul(seed_w_norm.view(self.num_aspects, self.num_seeds, 1))
             r = a_probs.view(-1, self.num_aspects, 1, 1).mul(a_emb_w).sum(dim=2).sum(dim=1)
@@ -185,31 +145,46 @@ class AspectAutoencoder(nn.Module):
             a_emb_w = self.a_emb.mul(self.seed_w.view(-1, self.num_aspects, self.num_seeds, 1))
             r = a_probs.view(-1, self.num_aspects, 1, 1).mul(a_emb_w).sum(dim=2).sum(dim=1)
 
-        return r, a_probs
+        # mask out of domain instances out of autoencoder branch
+        if self.training:
+            r_stack = torch.stack([enc, r], 0)
+            r = r_stack.gather(0, domain_mask.view(1, -1, 1).expand(1, -1, self.emb_size)).squeeze()
+
+        # domain classifier
+        x = self.dlin1(enc)
+        if self.drop is not None:
+            x = self.drop(x)
+        d_out = self.dlin2(x)
+
+        return r, d_out, a_probs
 
     def _create_neg_mask(self, batch_size):
         """Creates a mask for randomly selecting negative samples"""
         multi_weights = torch.ones(batch_size, batch_size) - torch.eye(batch_size)
         neg = min(batch_size - 1, self.neg_samples)
 
-        mask = torch.multinomial(multi_weights, neg)
-        mask = mask.unsqueeze(2).expand(batch_size, neg, self.emb_size)
-        mask = Variable(mask, requires_grad=False)
-        return mask
+        neg_mask = torch.multinomial(multi_weights, neg)
+        neg_mask = neg_mask.unsqueeze(2).expand(batch_size, neg, self.emb_size)
+        neg_mask = Variable(neg_mask, requires_grad=False)
+        return neg_mask
 
     def set_targets(self, module, input, output):
         """Sets positive and negative samples"""
-        assert self.cur_mask is not None, 'Tried to set targets without a mask'
+        assert self.cur_neg_mask is not None, 'Tried to set targets without a mask'
         batch_size = output.size(0)
 
         if torch.cuda.is_available():
-            mask = self.cur_mask.cuda()
+            neg_mask = self.cur_neg_mask.cuda()
         else:
-            mask = self.cur_mask
+            neg_mask = self.cur_neg_mask
 
-        self.negative = Variable(output.data).expand(batch_size, batch_size, self.emb_size).gather(1, mask)
         self.positive = Variable(output.data)
-        self.cur_mask = None
+        neg_in = Variable(output.data).expand(batch_size, batch_size, self.emb_size).gather(1, neg_mask)
+        neg_ood = Variable(-output.data.unsqueeze(1).expand_as(neg_in))
+        neg_stack = torch.stack([neg_ood, neg_in], 0)
+        self.negative = neg_stack.gather(0,
+                self.cur_domain_mask.view(1, -1, 1, 1).expand(1, -1, neg_in.size(1), neg_in.size(2))).squeeze()
+        self.cur_neg_mask = None
 
     def get_targets(self):
         assert self.positive is not None, 'Positive targets not set; needs a forward pass first'
@@ -223,13 +198,13 @@ class AspectAutoencoder(nn.Module):
             return self.a_emb.mean(dim=1)
 
     def train(self, mode=True):
-        super(AspectAutoencoder,  self).train(mode)
+        super(MultitaskAutoencoder,  self).train(mode)
         if self.encoder_hook is None:
             self.encoder_hook = self.seg_encoder.register_forward_hook(self.set_targets)
         return self
 
     def eval(self):
-        super(AspectAutoencoder, self).eval()
+        super(MultitaskAutoencoder, self).eval()
         if self.encoder_hook is not None:
             self.encoder_hook.remove()
             self.encoder_hook = None
@@ -240,27 +215,31 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(
       description =__doc__,
       formatter_class=argparse.RawDescriptionHelpFormatter)
-    parser.add_argument('data', help="Dataset name (without extension)", type=str)
+    parser.add_argument('data', help="Multi-domain dataset name (without extension)", type=str)
     parser.add_argument('--test_data', help="hdf5 file of test segments", type=str, default='')
     parser.add_argument('--min_len', help="Minimum number of non-stop-words in segment (default: 2)", type=int, default=2)
-    parser.add_argument('--aspects', help="Number of aspects (default: 10)", type=int, default=10)
     parser.add_argument('--aspect_seeds', help='file that contains aspect seed words (overrides number of aspects)',
             type=str, default='')
     parser.add_argument('--recon_method', help="Method of reconstruction (centr/init/fix/cos)",
             type=str, default='fix')
-    parser.add_argument('--kmeans', help="Aspect embedding initialization with kmeans", action='store_true')
-    parser.add_argument('--kmeans_iter', help="Number of times to re-run kmeans (default: 20)", type=int, default=20)
-    parser.add_argument('--attention', help="Use word attention", action='store_true')
     parser.add_argument('--negative', help="Number of negative samples (default: 20)", type=int, default=20)
-    parser.add_argument('--fix_w_emb', help="Fix word embeddings", action='store_true')
-    parser.add_argument('--fix_a_emb', help="Fix aspect embeddings", action='store_true')
-    parser.add_argument('--epochs', help="Number of epochs (default: 10)", type=int, default=15)
+    parser.add_argument('--attention', help="Type of word attention to use (default: none)", action='store_true')
+    parser.add_argument('--dropout', help="Dropout probability (default: 0)", type=float, default=0.0)
+    parser.add_argument('--binarize', help="Binarize domain labels", action='store_true')
+    parser.add_argument('--in_id', help="In-domain index (i.e., which is the domain of interest)", type=int, default=0)
+    parser.add_argument('--midlayer', help="Size of middle layer", type=int, default=50)
+    parser.add_argument('--fix_w_emb', help="Fix word embeddings (default: no)", action='store_true')
+    parser.add_argument('--fix_a_emb', help="Fix aspect embeddings (default: no)", action='store_true')
+    parser.add_argument('--epochs', help="Number of epochs (default: 15)", type=int, default=15)
     parser.add_argument('--lr', help="Learning rate (default: 0.001)", type=float, default=0.001)
+    parser.add_argument('--k', help="Domain classification loss coefficient (default: 1)", type=float, default=1)
+    parser.add_argument('--anneal_k', help="Anneal domain classification coefficient", action='store_true')
+    parser.add_argument('--start_k', help="Coefficient at start, if anneal_k=True (default: 10)", type=float, default=10)
+    parser.add_argument('--k_decay', help="Coefficient decay, if anneal_k=True (default: 0.5)", type=float, default=0.5)
     parser.add_argument('--l', help="Orthogonality loss coefficient (default: 1)", type=float, default=1)
     parser.add_argument('--savemodel', help="File to save model in (default: don't)", type=str, default='')
-    parser.add_argument('--semeval', help="Output file for evaluation (default: don't)", type=str, default='')
-    parser.add_argument('--sumout', help="Output file for summary generation (default: output.txt)",
-            type=str, default='output.txt')
+    parser.add_argument('--semeval', help="File to save semeval-style output (default: don't)", type=str, default='')
+    parser.add_argument('--sumout', help="File to save semeval-style output (default: don't)", type=str, default='')
     parser.add_argument('-q', '--quiet', help="No information to stdout", action='store_true')
     parser.add_argument('--seed', help="Random seed (default: system seed, -1)", type=int, default=-1)
     args = parser.parse_args()
@@ -283,12 +262,16 @@ if __name__ == "__main__":
 
     f = h5py.File(args.data + '.hdf5', 'r')
     batches = []
+    domains = []
     original = []
     scodes = []
+    num_domains = 0
     for b in f['data']:
         batches.append(Variable(torch.from_numpy(f['data/' +  b][()]).long()))
         original.append(list(f['original/' + b][()]))
         scodes.append(list(f['scodes/' + b][()]))
+        domains.append(Variable(torch.from_numpy(f['domains/' +  b][()]).long()))
+        num_domains = max(num_domains, domains[-1].data.max() + 1)
 
     w_emb_array = f['w2v'][()]
     w_emb = torch.from_numpy(w_emb_array)
@@ -308,17 +291,7 @@ if __name__ == "__main__":
             test_scodes.append(list(f['scodes/' + b][()]))
     f.close()
 
-
-    if args.kmeans:
-        # kmeans initialization (ABAE)
-        if not args.quiet:
-            print 'Running k-means...'
-        a_emb, _ = kmeans(w_emb_array, args.aspects, iter=args.kmeans_iter)
-        a_emb = torch.from_numpy(a_emb)
-        seed_w = None
-        args.num_seeds = None
-    elif args.aspect_seeds != '':
-        # seed initialization (MATE)
+    if args.aspect_seeds != '':
         fseed = open(args.aspect_seeds, 'r')
         aspects_ids = []
         if args.recon_method == 'fix' \
@@ -374,20 +347,22 @@ if __name__ == "__main__":
 
 
     if not args.quiet:
-        print 'Building model..'
+        print 'Building multitask model..'
 
-    net = AspectAutoencoder(vocab_size, emb_size,
+    net = MultitaskAutoencoder(vocab_size, emb_size,
             num_aspects=args.aspects, neg_samples=args.negative,
             w_emb=w_emb, a_emb=a_emb, recon_method=args.recon_method, seed_w=seed_w,
             num_seeds=args.num_seeds, attention=args.attention, fix_w_emb=args.fix_w_emb,
-            fix_a_emb=args.fix_a_emb)
+            fix_a_emb=args.fix_a_emb, num_domains=num_domains,
+            midlayer=args.midlayer, dropout=args.dropout)
 
     if torch.cuda.is_available():
         net = net.cuda()
 
-    rec_loss = TripletMarginCosineLoss()
+    rec_loss = TripletMarginCosineLoss(sum_loss=False)
     if not args.fix_a_emb: # orthogonality loss is only used when training aspect matrix
         orth_loss = OrthogonalityLoss()
+    crossent = nn.CrossEntropyLoss()
 
     params = filter(lambda p: p.requires_grad, net.parameters())
     optimizer = torch.optim.Adam(params, lr=args.lr)
@@ -398,35 +373,45 @@ if __name__ == "__main__":
 
     start_all = time()
 
+    if not args.anneal_k:
+        k = args.k
+    else:
+        k = args.start_k
+
     for epoch in range(args.epochs):
         if not args.quiet:
-            print 'Epoch', epoch+1,
+            print 'Epoch', epoch+1, '(k={0})'.format(k)
 
         start = time()
         perm = permutation(len(batches))
 
         for i in range(len(batches)):
             inputs = batches[perm[i]]
+            labels = domains[perm[i]]
 
             if inputs.shape[1] < args.min_len:
                 continue
 
             if torch.cuda.is_available():
                 inputs = inputs.cuda()
+                labels = labels.cuda()
 
-            out, a_probs = net(inputs, perm[i])
+            domain_mask = Variable((labels.data == args.in_id).long())
+            r, d_out, a_probs = net(inputs, domain_mask=domain_mask, batch_num=perm[i])
 
             positives, negatives = net.get_targets()
-            loss = rec_loss(out, positives, negatives)
+            loss_rec = rec_loss(r, positives, negatives)
+            loss_cl = crossent(d_out, labels)
+
+            loss = loss_rec + k * loss_cl
 
             if not args.fix_a_emb:
                 aspects = net.get_aspects()
                 loss += args.l * orth_loss(aspects)
 
-            
+            optimizer.zero_grad()
             loss.backward()
             optimizer.step()
-
 
         if args.semeval != '':
             semout = ''
@@ -444,7 +429,7 @@ if __name__ == "__main__":
                 inputs = inputs.cuda()
                 labels = labels.cuda()
 
-            _, a_probs = net(inputs, i)
+            _, _, a_probs = net(inputs, batch_num=i)
 
             for j in range(a_probs.size()[0]):
                 if args.semeval != '':
@@ -469,6 +454,10 @@ if __name__ == "__main__":
             fsum.close()
 
         net.train()
+        fsum.close()
+
+        if args.anneal_k:
+            k *= args.k_decay
 
         if not args.quiet:
             print '({0:6.2f}sec)'.format(time() - start)
